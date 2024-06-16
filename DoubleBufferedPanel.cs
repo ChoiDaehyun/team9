eb7537892a746a8a7025a7597a75fe170e35e1f5
ym kim <112836816+maru000000@users.noreply.github.com>
2024-06-16 15:15:05 +0900

    Add files via upload

---
+++ DoubleBufferedPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suika_Game
+{
+    public class DoubleBufferedPanel : System.Windows.Forms.Panel
+    {
+        public DoubleBufferedPanel() { 
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer
+        | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+            this.UpdateStyles();
+        
+        }
+    }
+}
